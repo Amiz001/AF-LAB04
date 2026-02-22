998e1d88b7b3e2b8d25714d43a204a0650029ed7
Amiz001 <lgmalshan@gmail.com>
2026-02-22 11:29:21 +0530

    web server added

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const http = require('http');
 
 console.log("App.js created!");
 
@@ -11,3 +12,10 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
  if (err) throw err;
  console.log('File saved!');
 });
+
+
+http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write('Hello World!');
+ res.end();
+}).listen(8080);
